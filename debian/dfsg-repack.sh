5c00ce050853ffd8bbb111a1dda0087c8190420d
Ondřej Surý <ondrej@sury.org>
2013-07-19 15:03:38 +0200

    Replace docbook icons with icons from docbook-xsl

---
+++ debian/dfsg-repack.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+if [ ! -f "$3" ] && [ ! -f "$1" ]; then
+    echo "This script must be run via uscan or by manually specifying the tarball" >&2
+    exit 1
+fi
+
+tarball=
+
+[ -f "$3" ] && tarball="$3"
+[ -z "$tarball" -a -f "$1" ] && tarball="$1"
+
+fname="$(basename "$tarball")"
+tarball="$(readlink -f "$tarball")"
+
+tdir="$(mktemp -d)"
+trap '[ ! -d "$tdir" ] || rm -r "$tdir"' EXIT
+
+xzcat "$tarball" | \
+    tar --wildcards \
+        --delete '*/guides/assets/images/icons/*' \
+        --delete '*/guides/assets/images/icons' \
+    > "$tdir/${fname/.xz}"
+xz "$tdir/${fname/.xz}"
+
+mv "$tdir/$fname" "${tarball/.tar.xz}+dfsg.tar.xz"
